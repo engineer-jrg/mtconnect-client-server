77dfba7febc1eb6950f3301dece8a179aa88df35
Juan Rodriguez Gasca <engineer.jrg@gmail.com>
2020-04-26 12:50:30 -0500

    :bug: Manejar excepción al cargar archivo de configuración

--- config/environment/config.js
+++ config/environment/config.js
@@ -5,8 +5,12 @@ const Schema = require('../environment/schema');
 
 const config = Convict(Schema);
 const mode = config.get('env');
-const configPath = Path.resolve(__dirname, './.env.' + mode + '.json');
-config.loadFile(configPath);
-config.validate();
+try {
+    const configPath = Path.resolve(__dirname, './.env.' + mode + '.json');
+    config.loadFile(configPath);
+    config.validate();
+} catch (error) {
+    console.log('* Ejecutandoce con la configuración por defecto');
+}
 
 module.exports = config;
